807d811f4cdf2021b20f1851a9e0763c748b8176
youssefrramdan <youfahmed353@gmail.com>
2025-02-05 16:43:47 +0200

    edit Email

--- middlewares/emailTemplate.js
+++ middlewares/emailTemplate.js
@@ -68,7 +68,7 @@ export const emailTemplate = (token) => {
           <div class="message">
               Thank you for signing up! Please confirm your email address by clicking the button below.
           </div>
-          <a href="https://pflow.onrender.com/api/v1/auth/verify/${token}" class="verify-button">Verify Email</a>
+          <a href="https://agricultural-emmaline-youssef-ramadan-899e2b27.koyeb.app/api/v1/auth/verify/${token}" class="verify-button">Verify Email</a>
           <div class="footer">
               <p>If you did not create an account, please ignore this email or <a href="#">contact support</a>.</p>
               <p>Best regards,<br>The [Your Company] Team</p>
