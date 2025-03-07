ec8f353982ceea1175c4296ac0d07741c62542a4
Youssef Ramadan <youfahmed353@gmail.com>
2025-03-07 23:21:08 +0200

    add cart model

---
+++ models/Cart.model.js
@@ -0,0 +1,43 @@
+import { model, Schema, Types } from "mongoose";
+
+const cartSchema = new Schema(
+  {
+    items: [
+      {
+        inventory: {
+          type: Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        products: [
+          {
+            drug: {
+              type: Types.ObjectId,
+              ref: "Drug",
+              required: true,
+            },
+            quantity: {
+              type: Number,
+              required: true,
+              min: [1, "Quantity must be at least 1"],
+            },
+            price: {
+              type: Number,
+              required: true,
+            },
+          },
+        ],
+      },
+    ],
+    pharmacy: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    totalCartPrice: Number,
+    totalPriceAfterDiscount: Number,
+  },
+  { timestamps: true }
+);
+export default model("Cart", cartSchema);
+
