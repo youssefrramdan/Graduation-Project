1165320a7599bd060272f4952a030a738dccaa87
Youssef Ramadan <elhenawyaya212gmail.com>
2025-03-09 22:13:40 +0200

    edit 2

--- controllers/drug.controller.js
+++ controllers/drug.controller.js
@@ -400,7 +400,7 @@ const getAllDrugsForSpecificInventory = asyncHandler(async (req, res, next) => {
   const countDocuments = await DrugModel.countDocuments(filter);
 
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const limit = Number(req.query.limit) || 60;
   const skip = (page - 1) * limit;
 
   const pagination = {
