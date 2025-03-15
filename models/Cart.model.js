75368a30c46ee4a35bc5307da9d1a560d3b0a0f8
youssefrramdan <youfahmed353@gmail.com>
2025-03-15 14:39:13 +0200

    edit setting for frontend

--- models/Cart.model.js
+++ models/Cart.model.js
@@ -1,5 +1,4 @@
 import { model, Schema, Types } from "mongoose";
-import DrugModel from "./Drug.model.js";
 
 const cartSchema = new Schema(
   {
@@ -53,22 +52,5 @@ const cartSchema = new Schema(
   { timestamps: true }
 );
 
-cartSchema.pre("save", async function (next) {
-  try {
-    for (const item of this.items) {
-      for (const drugItem of item.drugs) {
-        const drugDetails = await DrugModel.findById(drugItem.drug);
-        if (!drugDetails) {
-          throw new Error(`Drug with id ${drugItem.drug} not found`);
-        }
-        drugItem.price = drugDetails.price;
-        drugItem.discountedPrice = drugDetails.discountedPrice;
-      }
-    }
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
 
 export default model("Cart", cartSchema);
