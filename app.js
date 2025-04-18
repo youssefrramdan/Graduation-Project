69fd640c292932566735d1992d35554a1209c9ea
youssefrramdan <youfahmed353@gmail.com>
2025-04-18 14:37:56 +0200

    Refactor: Move error handling for unhandled rejections and uncaught exceptions to server.js and clean up app.js

--- app.js
+++ app.js
@@ -53,21 +53,5 @@ app.all("*", (req, res, next) => {
 
 app.use(globalError);
 
-process.on("unhandledRejection", (err) => {
-  console.error(`unhandledRejection Error : ${err.name} | ${err.message}`);
-  server.close(() => {
-    console.error(`Shutting down ...`);
-    process.exit(1);
-  });
-});
-// What about errors that happen synchronously outside Express?
-// For example, if an error occurs before Express starts,
-//  it won't be caught by Express error handling middleware.
-
-process.on("uncaughtException", (err) => {
-  console.error(`Uncaught Exception: ${err.name} | ${err.message}`);
-  // Gracefully shutting down
-  process.exit(1); // Exit immediately with failure code
-});
 
 export default app;
--- server.js
+++ server.js
@@ -3,7 +3,25 @@ import databaseConnection from "./config/database.config.js";
 
 databaseConnection();
 
-const PORT = process.env.PORT || 8000 ;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT, () => {
   console.log(`server is running on port ${PORT} ...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error(`unhandledRejection Error : ${err.name} | ${err.message}`);
+  server.close(() => {
+    console.error(`Shutting down ...`);
+    process.exit(1);
+  });
+});
+
+// What about errors that happen synchronously outside Express?
+// For example, if an error occurs before Express starts,
+//  it won't be caught by Express error handling middleware.
+
+process.on("uncaughtException", (err) => {
+  console.error(`Uncaught Exception: ${err.name} | ${err.message}`);
+  // Gracefully shutting down
+  process.exit(1); // Exit immediately with failure code
 });
