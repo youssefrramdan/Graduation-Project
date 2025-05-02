c68e8f255f7e0d89f2941a7642312d2196eff53e
youssefrramdan <youfahmed353@gmail.com>
2025-05-02 16:28:30 +0300

    Enhance: Add getAllCategories and getSpecificCategory routes

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,14 +1,24 @@
 import express from "express";
 import createUploader from "../middlewares/uploadImageMiddleware.js";
-import { createCategory, deleteCategory, updateCategory } from "../controllers/category.controller.js";
+import {
+  createCategory,
+  deleteCategory,
+  getAllCategories,
+  getSpecificCategory,
+  updateCategory,
+} from "../controllers/category.controller.js";
 
 const categoryRouter = express.Router();
 const upload = createUploader("category-image");
 
-categoryRouter.route("/").post(upload.single("imageCover"), createCategory);
+categoryRouter
+  .route("/")
+  .post(upload.single("imageCover"), createCategory)
+  .get(getAllCategories);
 
 categoryRouter
   .route("/:id")
+  .get(getSpecificCategory)
   .put(upload.single("imageCover"), updateCategory)
   .delete(deleteCategory);
 
